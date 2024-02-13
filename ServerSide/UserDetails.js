ae8eb4299d1103797bd697c9eed4e4d08df49365
Nur Alibasyah <nuralibasyah@gmail.com>
2024-02-13 13:37:50 +0700

    updated UserDetails in backend

--- ServerSide/UserDetails.js
+++ ServerSide/UserDetails.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const UserDetailSchema = new mongoose.Schema({
-    name: String,
-    email:{type: String, unique:true},
-    password: String,    
+    name:String,
+    email: {type:String, unique: true},
+    password:String,
 },
 {
     collection:"UserInfo",
 }
 );
-mongoose.model("userInfo",UserDetailSchema);
+mongoose.model("UserInfo", UserDetailSchema);
