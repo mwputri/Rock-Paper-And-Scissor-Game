ef8ce2f038ceef012d0a00a18e2d2fcb62a0182f
Nur Alibasyah <nuralibasyah@gmail.com>
2024-02-14 22:24:14 +0700

    added totalScore to userDetails and changed mongoUrl

--- ServerSide/UserDetails.js
+++ ServerSide/UserDetails.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserDetailSchema = new mongoose.Schema({
-    name:String,
-    email: {type:String, unique: true},
-    password:String,
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    totalScore: { type: Number, required: true },
 },
 {
     collection:"UserInfo",
