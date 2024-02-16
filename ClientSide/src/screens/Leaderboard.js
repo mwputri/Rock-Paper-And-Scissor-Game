c58ba50bfd079ee92e12bba0594ac0fea3362bd2
Nur Alibasyah <nuralibasyah@gmail.com>
2024-02-16 18:36:44 +0700

    updated leaderboard js and cors in backend

--- ClientSide/src/screens/Leaderboard.js
+++ ClientSide/src/screens/Leaderboard.js
@@ -1,103 +1,102 @@
-import React from "react";
-import {
-  FlatList,
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect }  from 'react';
+import { FlatList, View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
-const Leaderboard = ({}) => {
+const Leaderboard = ({ }) => {
   const [userData, setUserData] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+useEffect(() => {
+  fetchData();
+}, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/leaderboards");
-      const userData = response.data.sort(
-        (a, b) => parseInt(b.totalScore) - parseInt(a.totalScore)
-      );
-      setUserData(userData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-  const navigation = useNavigation();
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/leaderboards');
+    const userData = response.data.sort((a, b) => parseInt(b.totalScore) - parseInt(a.totalScore));
+    setUserData(userData);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
-  const Item = ({ data, index }) => (
-    <View style={styles.item}>
-      <Text style={[styles.itemText, { marginRight: 10 }]}>
-        {index === 0 ? (
-          <Image
-            source={require("../../assets/Winner.png")}
-            style={styles.winnerImage}
-          />
-        ) : (
-          index + 1
-        )}
-      </Text>
-      <Text style={[styles.itemText, { marginRight: 160 }]}>{data.nama}</Text>
-      <Text style={styles.itemText}>{data.score}</Text>
-    </View>
-  );
+    const navigation = useNavigation();
 
-  return (
-    <View style={styles.container}>
-      <View style={{ flex: 1 }}>
-        <TouchableOpacity
-          style={[styles.profileButton, { alignItems: "flex-end" }]}
-          onPress={() => navigation.navigate("Profile")}
-        >
-          <Image
-            source={require("../../assets/Profile.png")}
-            style={styles.profileIcon}
-          />
-        </TouchableOpacity>
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <Image
-            source={require("../../assets/Leaderboard.png")}
-            style={styles.image}
-          />
-          <Text style={styles.title}>LEADERBOARD</Text>
-
-          <FlatList
-            data={sortedData}
-            renderItem={({ item, index }) => <Item data={item} index={index} />}
-            keyExtractor={(item) => item.nama}
-          />
+    const Item = ({ data, index }) => (
+        <View style={styles.item}>
+          <Text style={[styles.itemText,{marginRight:10}]}>
+            {index === 0 ? <Image source={require('../../assets/Winner.png')} style={styles.winnerImage} /> 
+            : index + 1}
+            </Text>
+          <Text style={[styles.itemText,{marginRight:120}]}>{data.name}</Text>
+          <Text style={styles.itemText}>{data.totalScore}</Text>
         </View>
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <TouchableOpacity
-            style={[styles.button, { elevation: 5 }]}
-            onPress={() => {
-              "";
-            }}
-          >
-            <Text style={styles.buttonText}>Let's Play</Text>
-          </TouchableOpacity>
-        </View>
+      );  
+
+    return (
+    <ScrollView >
+      <View>
+ {/* <TouchableOpacity style={[styles.profileButton, {alignItems:'flex-end'}]} onPress={() => navigation.navigate('Profile')}>
+        <Image source={require('../assets/Profile.png')} style={styles.profileIcon} />
+      </TouchableOpacity> */}
+      <TouchableOpacity style={styles.logoutButton} onPress={() => navigation.navigate('Profile')}>
+        <Text style={ [styles.buttonText, {fontWeight:'normal', fontSize:'14'}] }>Logout</Text>
+      </TouchableOpacity>
       </View>
-    </View>
+      
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Image source={require('../../assets/Leaderboard.png')} style={styles.image} />
+        <Text style={styles.title}>LEADERBOARD</Text>
+        
+        <View style={styles.header}>
+          <Text style={[styles.headerText, {marginRight:35}]}>   </Text>
+          <Text style={[styles.headerText, {marginRight:140}]}>Name</Text>
+          <Text style={styles.headerText}>Score</Text>
+        </View>
+
+        <FlatList
+          data={userData}
+          renderItem={({ item, index }) => <Item data={item} index={index} />}
+          keyExtractor={item => item._id}
+        />        
+      </View>
+      
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity
+        style={[styles.button, {elevation:5}]}
+        onPress={() => {''}}
+      >
+        <Text style={ styles.buttonText } onPress={() => navigation.navigate('Round')}>Let's Play</Text>
+      </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  profileButton: {
-    // position: 'absolute',
-    paddingTop: 60,
-    paddingRight: 20,
-    padding: 10,
+  logoutButton: {
+    width:80,
+    height: 30,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'red',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    position:'absolute',
+    top: 10,
+    right:10,
   },
   profileIcon: {
     width: 30,
@@ -108,50 +107,50 @@ const styles = StyleSheet.create({
     width: 120,
     marginTop: 142,
   },
-  winnerImage: {
+  winnerImage:{
     height: 25,
     width: 25,
-    alignItems: "flex-start",
+    // alignItems:'flex-start',
   },
   title: {
     fontSize: 32,
-    fontWeight: "700",
-    textAlign: "center",
-    marginTop: 20,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginTop:20,
   },
   header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingHorizontal: 20,
     marginBottom: 10,
   },
   headerText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 18,
-    color: "#333",
-    marginTop: 20,
+    color: '#333',
+    marginTop:20,
   },
   item: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingHorizontal: 20,
     borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+    borderBottomColor: '#ccc',
     paddingVertical: 10,
   },
   itemText: {
     fontSize: 16,
-    color: "#333",
+    color: '#333',
   },
   button: {
     width: 162,
     height: 58,
     borderRadius: 25,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     marginTop: 40,
-    backgroundColor: "#F6B17A",
-    shadowColor: "#000",
+    backgroundColor:'#F6B17A',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 3,
@@ -159,12 +158,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.27,
     shadowRadius: 4.65,
   },
-  buttonText: {
+  buttonText:{
     // fontFamily:'Roboto',
-    color: "#FFF",
-    fontSize: 20,
-    fontWeight: 700,
-  },
+    color:'#FFF',
+    fontSize:20,
+    fontWeight:700,
+  }
 });
 
 export default Leaderboard;
