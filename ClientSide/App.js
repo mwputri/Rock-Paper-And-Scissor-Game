581b55b86d4e57d748ab6d89d9e5a6b0c39d670b
mw putri <mwputrimeiliniahp@gmail.com>
2024-02-18 01:19:37 +0700

    update app.js add navigation Main & RondeModal

--- ClientSide/App.js
+++ ClientSide/App.js
@@ -6,36 +6,50 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
-import Main from "./src/screens/Main";
+import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import Leaderboard from "./src/screens/Leaderboard";
+import RondeModal from "./src/screens/RondeModal";
+import Main from "./src/screens/Main";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-// const App = () => {
-//   return <Home />;
-// };
-// export default App;
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Main/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Leaderboard"
+          component={Leaderboard}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+        name="RondeModal"
+        component={RondeModal}
+        options={{ headerShown: false }}
+      />
+        <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{ headerShown: false }}
+      />
+      </Stack.Navigator>
     </NavigationContainer>
 
-    // <Home />
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FEF6E1",
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-});
